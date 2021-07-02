5fcb3292bb1e4ce626eca196ff3d6013d89ff935
ghdj-story <sarah001015@naver.com>
2021-07-03 00:10:09 +0900

    honor link error correction

--- dbgproject/dbg/urls.py
+++ dbgproject/dbg/urls.py
@@ -13,14 +13,11 @@ urlpatterns = [
     path('aboutUs/',aboutUs, name="aboutUs"),
     path('searchMap/',searchMap,name="searchMap"),
     path('searchResult/',searchResult, name='searchResult'),
-<<<<<<< HEAD
     path('mypage/',mypage,name="mypage"),
     path('mypageDiary/',mypageDiary,name="mypageDiary"),
     path('mypagePhoto/',mypagePhoto,name="mypagePhoto"),
     path('test/',test,name="test"),path('enroll/',enroll, name="enroll"),
-=======
     path('enroll/',enroll, name="enroll"),
->>>>>>> dfc974d6c9c8217de9bf4a16a9a5367c52f3a74e
     path('enroll2/',enroll2, name="enroll2"),
     path('caaard/', caaard, name="caaard"),
 ] 
