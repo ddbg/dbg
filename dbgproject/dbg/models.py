17c3b8e55d530470ff886c5b35a3c2ca09ec18e0
angly97 <angly97@naver.com>
2021-07-28 20:22:38 +0900

    import models in view.py

--- dbgproject/dbg/models.py
+++ dbgproject/dbg/models.py
@@ -4,7 +4,7 @@ from django.db import models
 from django.db.models.fields import DateField
 from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
 class User(models.Model):
     user_name=models.CharField(max_length=50)                                       #실명
     user_nickname=models.CharField(max_length=50)                                   #닉네임
@@ -17,27 +17,23 @@ class User(models.Model):
    
 class Animal(models.Model):
 
-    animal_id = models.IntegerField(primary_key=True)                   # 동물 고유 id
+    animal_id = models.IntegerField(primary_key=True)                               # 동물 고유 id
     
-    name = models.CharField(max_length=50)          
-    owner_id = models.ForeignKey(User, on_delete=models.CASCADE)        # 주인 id
-    species = models.CharField(max_length=50)                           # 상위 종류
-    subspecies = models.CharField(max_length=50)                        # 하위 종류
-    profile_photo = models.ImageField(upload_to='static/profile_photo/', null=True)
-    birthday = models.DateField()                                       # 탄생일
-    memorialday = models.DateField()                                    # 제삿날
-    introduce = models.TextField()
-    season = models.CharField(max_length=10)                            # 꽃다발 이름 저장될 배열                         # 제일 좋아한 계절
-    flowers = ArrayField(
+    name = models.CharField(max_length=50)                                          # 동물 이름
+    owner_id = models.ForeignKey(User, on_delete=models.CASCADE)                    # 주인 id
+    species = models.CharField(max_length=50)                                       # 상위 종류
+    subspecies = models.CharField(max_length=50)                                    # 하위 종류
+    profile_photo = models.ImageField(upload_to='static/profile_photo/', null=True) # 프로필 사진
+    birthday = models.DateField()                                                   # 탄생일
+    memorialday = models.DateField()                                                # 제삿날
+    introduce = models.TextField()                                                  # 소개글
+    season = models.CharField(max_length=10)                                        # 제일 좋아한 계절                         
+    flowers = ArrayField(                                                           # 꽃다발 이름 저장될 배열 
             models.CharField(max_length=10, blank=True),
             size=8
             ) 
-    gravestone = models.CharField(max_length=50)                        # 묘비
+    gravestone = models.CharField(max_length=50)                                    # 묘비
     
-    # models.py의 Blog 클래스 내부 함수
-    def __str__(self):
-        # 객체 호출 시, 포스팅 제목을 오브젝트 명으로
-        return self.name
 
   
 <<<<<<< HEAD
