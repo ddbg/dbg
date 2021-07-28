4da296007bf423b02e11f3b31bcca8bbb2e1aec1
yyjun1649 <yy164946@naver.com>
2021-07-28 14:37:18 +0900

    Add 하루를들려줄게, 갤러리, 방명록

--- dbgproject/dbg/models.py
+++ dbgproject/dbg/models.py
@@ -30,14 +30,34 @@ class Animal(models.Model):
         # 객체 호출 시, 포스팅 제목을 오브젝트 명으로
         return self.name
 
-
-    # 방명록
-    
-    # 하루를 들려줄게
-
-    # 갤러리
-
   
-
+class Diary(models.Model): # 하루를 들려줄게
+    owner_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    animal_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    pub_date = models.DateTimeField()
+    body=models.TextField()
     
-     
+    def __str__(self):
+        return self.owner_id + " " + self.title + "\n" + self.body
+    
+    
+class Gallery(models.Model): # 갤러리
+    owner_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    animal_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to = "gallery/", blank = True, null = True)
+    
+    def __str__(self):
+        return self.image
+    
+class VisiterBook(models.Mode): #방명록
+    owner_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    visitor_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    animal_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    pub_date = models.DateTimeField()
+    body=models.TextField()
+    reply=models.TextField()
+    
+    def __str__(self):
+        return self.visitor_id + self.title + "\n" + self.body
--- dbgproject/dbgproject/urls.py
+++ dbgproject/dbgproject/urls.py
@@ -17,11 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 
 from dbg.views import home
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name="home"),
 
     path('dbg/', include('dbg.urls')),
-
-]
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
