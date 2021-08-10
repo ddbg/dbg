17d2b95923798818b70d9aad296722357e892f46
ghdj-story <sarah001015@naver.com>
2021-08-11 02:58:45 +0900

    signUp ,login view link with html

--- dbgproject/common/views.py
+++ dbgproject/common/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.forms import AuthenticationForm,UserCreationForm
 from django.contrib.auth import authenticate,login,logout
 from .models import * 
+from dbg.models import *
+from django.contrib import auth
 
 # Create your views here.
 
@@ -28,12 +30,18 @@ def logout_view(request):
 
 def signUp(request):
      if request.method == 'POST':
-         form=AuthenticationForm(request=request,data = request.POST)
-         if form.is_valid():
-             user = form.save()
-             login(request,user)
-         return redirect("home")
-             
+         if request.POST['password1']== request.POST['password2']:
+            user = User.objects.create_user(
+                user_name=request.POST['name'],                #실명
+                user_nickname=request.POST['nickname'],        #닉네임
+                Username=request.POST['id'],                   #이메일
+                Password=request.POST['password1'],            #비밀번호
+                user_phone_number=request.POST['phone_number'],   #전화번호
+                user_link=request.POST['link_linkaccount'],           #계정
+            )
+            auth.login(request.user)
+            return redirect("home")
+            
      else:
          form: UserCreationForm
          return render(request, "signUp.html")
--- dbgproject/dbgproject/settings.py
+++ dbgproject/dbgproject/settings.py
@@ -28,6 +28,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+AUTH_USER_MODEL='dbg.User'
+
 
 # Application definition
 
